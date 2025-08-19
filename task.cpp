a5da99717de82b57125e34ffa88ba879759e37c9
JoshikaPsiog <joshika.mahendrababu@psiog.com>
2025-08-19 15:48:31 +0530

    datas updated in csv file

--- task.cpp
+++ task.cpp
@@ -1,36 +1,85 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-bool isValidDateFormat(const string& date) {
-    if (date.length() != 10) return false;
-    if (date[4] != '/' || date[7] != '/') return false;
-    for (int i = 0; i < date.length(); ++i) {
-        if (i == 4 || i == 7) continue;
-        if (!isdigit(date[i])) return false;
+void inputs();
+
+bool isValidDateFormat(const string &date)
+{
+    if (date.length() != 10)
+        return false;
+    if (date[4] != '/' || date[7] != '/')
+        return false;
+    for (int i = 0; i < date.length(); ++i)
+    {
+        if (i == 4 || i == 7)
+            continue;
+        if (!isdigit(date[i]))
+            return false;
     }
- 
     return true;
 }
-void input(){
-    string date;
-    cout<<" Enter Unit Price Date (YYYY/MM/DD) :";
-    getline(cin,date);
-    if(!isValidDateFormat(date)){
-            cout<<"invalid date";
-            return;
+
+void inputs()
+{
+    char input;
+    string date, item_name;
+    double unit_Price, total;
+    int item_quantity;
+
+    cout << " Enter Unit Price Date (YYYY/MM/DD) :";
+    getline(cin, date);
+
+    if (!isValidDateFormat(date))
+    {
+        cout << "invalid date";
+        return;
+    }
+
+    cout << "Enter Item Name: ";
+    getline(cin, item_name);
+    cout << "Enter Unit Price: ";
+    cin >> unit_Price;
+    cout << "Enter Item Quantity: ";
+    cin >> item_quantity;
+    cin.ignore();
+
+    total = item_quantity * unit_Price;
+
+    ofstream file("sales.csv", ios::app);
+    if (!file)
+    {
+        cerr << "Error opening sales.csv\n";
+        return;
+    }
+
+    file << date << "," << item_name << "," << unit_Price << "," << item_quantity << "," << total << "\n";
+    file.close();
+
+    cout << "do you want to continue?(y/n): ";
+    cin >> input;
+    cin.ignore();
+
+    if (input == 'Y' || input == 'y')
+    {
+        inputs();
     }
 }
-void getdata(){
-    ofstream files1("sales.csv");
-   if(files1.is_open()){
-     input();
-   }
-   else{
-    ofstream createFile("sales.csv");
-   }
-    
+
+void getdata()
+{
+    ifstream checkFile("sales.csv");
+    if (!checkFile)
+    {
+        ofstream createFile("sales.csv");
+        createFile.close();
+    }
+    checkFile.close();
+
+    inputs();
 }
-int main(){
+
+int main()
+{
     getdata();
 }
